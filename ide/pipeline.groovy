36b2b9bcf0675976c26b2e4c1040d484fc77a88a
Andrew Tan <andrewwtan@gmail.com>
2022-07-11 11:00:48 -0700

    Enabled mailhog

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -4,20 +4,20 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
+                git url: 'https://github.com/andrewwtan/jgsu-spring-petclinic.git', branch: 'main'
             }            
         }
         stage('Build') {
             steps {
-                sh './mvnw clean package'
-                //sh 'false' // true
+                sh './mvnw clean compile'
+                // sh 'false' // true
             }
         
             post {
-                always {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
-                }
+                // always {
+                //     // junit '**/target/surefire-reports/TEST-*.xml'
+                //     // archiveArtifacts 'target/*.jar'
+                // }
                 changed {
                     emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
                         body: "Please go to ${BUILD_URL} and verify the build", 
